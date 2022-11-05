e8e9b66555ca4dacd93b1dd753ac1e3116b8d723
NzM <nazimfilzer@gmail.com>
2022-11-05 21:00:38 +0530

    Deleted products

--- src/controllers/products/index.js
+++
@@ -1,7 +0,0 @@
-const addProduct = require('./addProduct')
-const deleteProduct = require('./deleteProduct')
-const getAllProducts = require('./getAllProducts')
-const getProduct = require('./getProduct')
-const updateProduct = require('./updateProduct')
-
-module.exports = { ...addProduct, ...deleteProduct, ...getAllProducts, ...getProduct, ...updateProduct }
