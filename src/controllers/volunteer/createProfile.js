369999d911338159be161157471dea205a5f6f0a
NzM <nazimfilzer@gmail.com>
2022-11-05 22:07:38 +0530

    Sate

--- src/controllers/volunteer/createProfile.js
+++ src/controllers/volunteer/createProfile.js
@@ -2,8 +2,8 @@ const User = require('../../models/User')
 
 const createProfile = async (req, res) => {
     try {
-        const { phoneNumber, name, city } = req.body;
-        const user = await User.findOneAndUpdate({ _id: req.user._id }, { phoneNumber, name, city }, { new: true });
+        const { phoneNumber, name, city,state } = req.body;
+        const user = await User.findOneAndUpdate({ _id: req.user._id }, { phoneNumber, name, city,state }, { new: true });
         res.status(201).json({
             message: 'Profile Created',
             user: user,
